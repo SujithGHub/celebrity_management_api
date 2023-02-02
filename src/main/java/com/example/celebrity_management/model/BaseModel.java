394abd4037bb3a6e09aaf53beb076d254ecbe494
sujithgit21 <sujithrmsh@gmail.com>
2023-02-02 17:28:14 +0530

    tables created

--- src/main/java/com/example/celebrity_management/model/BaseModel.java
+++ src/main/java/com/example/celebrity_management/model/BaseModel.java
@@ -1,11 +1,12 @@
 package com.example.celebrity_management.model;
 
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
 import java.util.Date;
 import lombok.Data;
 import org.hibernate.annotations.GenericGenerator;
-import org.springframework.data.annotation.Id;
+
 
 @Data
 @MappedSuperclass
@@ -14,7 +15,6 @@ public class BaseModel {
   @Id
   @GeneratedValue(generator = "system-uuid")
   @GenericGenerator(name = "system-uuid", strategy = "uuid")
-  
   private String id;
   private Date updatedDate;
   private Date createdDate;
--- src/main/java/com/example/celebrity_management/model/CelebrityModel.java
+++ src/main/java/com/example/celebrity_management/model/CelebrityModel.java
@@ -1,14 +1,12 @@
 package com.example.celebrity_management.model;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 @Data
 @Entity
 @EqualsAndHashCode(callSuper = false)
-@Table(name = "celebrity")
 public class CelebrityModel extends BaseModel {
 
   private String name;
