4bf458fe64f5db09d8d2fdeb9dd68cccf1f61939
sujithgit21 <sujithrmsh@gmail.com>
2023-02-02 15:41:51 +0530

    api initial commit

---
+++ src/main/java/com/example/celebrity_management/CelebrityManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.celebrity_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CelebrityManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CelebrityManagementApplication.class, args);
+	}
+
+}
